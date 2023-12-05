aa68df8a3ec374381eb96bbad737351f37f2830f
M <MungkornHorpanich@gmail.com>
2023-12-05 20:06:02 +0700

    Finishing everthing

--- api/index.js
+++ api/index.js
@@ -15,7 +15,9 @@ app.use(
 );
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URL);
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("MongoDB connected"));
 
 app.get("/test", async (req, res) => {
   res.json("test ok");
