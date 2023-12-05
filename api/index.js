054f463380317edc0641d6b5be501b86c9ffefd4
M <MungkornHorpanich@gmail.com>
2023-12-05 21:21:59 +0700

    Fixing the cors

--- api/index.js
+++ api/index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(
   cors({
     credentials: true,
-    origin: "https://edgegambit.netlify.app/",
+    origin: "https://edgegambit-cinnamonrolly.vercel.app/",
   })
 );
 app.use(express.json());
